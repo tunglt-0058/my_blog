9c29451e24e152770f874e42c4732c428f221d54
Le Tat Tung <letattungtb@gmail.com>
2019-03-23 13:38:12 +0900

    post detail

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 ## Template
 - [startbootstrap-clean-blog](https://github.com/BlackrockDigital/startbootstrap-clean-blog).
-##Infinite Scrolling
+## Infinite Scrolling
 - [Infinite Scrolling](https://www.driftingruby.com/episodes/infinite-scrolling).
-
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,18 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: :show
   def index
     @posts = Post.paginate(page: params[:page], per_page: Settings.page_posts_size).order(id: :desc)
+  end
+
+  def show
+  end
+
+  private
+  def find_post
+    @post = Post.find(params[:id])
+    unless @post
+      flash[:danger] = t"not_found.post"
+      redirect_to not_found_index_path
+    end
   end
 end
