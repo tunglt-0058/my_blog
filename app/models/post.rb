796a593413b7da66c622f58b4ae6d8391a8fcdc4
Le Tat Tung <letattungtb@gmail.com>
2019-03-23 15:36:27 +0900

    custom URL

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,13 @@
 class Post < ApplicationRecord
   has_many :post_categories
   has_many :categories, through: :post_categories, dependent: :destroy
+  before_save :to_slug
+  def to_param
+    slug
+  end
+
+  private
+  def to_slug
+    self.slug = self.title.parameterize.truncate(80, omission: "")
+  end
 end
